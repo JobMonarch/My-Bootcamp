ddd3bd08e1631e306b8ff78ef0b0a39f9d123dd4
Yusuf Ozdemir <ozdemiryusuf677@gmail.com>
2026-04-08 14:48:49 +0300

    Finish Activity_1

--- src/main/java/module2/Activity_1.java
+++ src/main/java/module2/Activity_1.java
@@ -1,4 +1,13 @@
 package module2;
 
 public class Activity_1 {
+    public static void main(String[] args) {
+        int number = 0; // our starting value
+
+        System.out.println("Before Update: "+ number); //prints the value before the update
+
+        number = 25; // updating the value
+
+        System.out.println("After Update: " + number); // prints the value
+    }
 }
